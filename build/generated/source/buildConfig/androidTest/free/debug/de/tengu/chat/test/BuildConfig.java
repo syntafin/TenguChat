036f0565b652ee4d369ee8b95e9dbe2cc6abb4b8
Syntafin <info@tengu.email>
2016-08-21 19:30:55 +0200

    Bugfix merged

--- build/generated/source/buildConfig/androidTest/free/debug/de/tengu/chat/test/BuildConfig.java
+++ build/generated/source/buildConfig/androidTest/free/debug/de/tengu/chat/test/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "de.tengu.chat.test";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "free";
-  public static final int VERSION_CODE = 120;
-  public static final String VERSION_NAME = "1.0.1";
+  public static final int VERSION_CODE = 130;
+  public static final String VERSION_NAME = "1.0.2";
 }
